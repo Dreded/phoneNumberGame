f1061bb494b5261c8da7d5bd9b596d0ef40b901c
Ryan H <dreded@Dredx.com>
2023-01-06 11:32:58 -0800

    added support for secrets file and multiple #s

--- phone.py
+++ phone.py
@@ -2,16 +2,37 @@
 import random
 import os
 import getch
+import yaml
+
+phoneNumbers = [
+    {'name': "Bob Loblaw"},{'number': '555-555-5555'},
+    {'name': "Test"},{'number': '555-555-5555'},
+]
+attempts = 4
+difficulty = 9
+
 
 def clearScreen():
     os.system('cls' if os.name == 'nt' else 'clear')
 
-number = [5,5,5,5,5,5,5,5,5,5]
-name = 'Bob Loblaws'
-attempts = 4
-difficulty = 9
+def fileExists(file):
+    return os.path.exists(file)
 
-def getMissing(random_count, missingNums = None):
+def loadSecretsFile():
+    secretsFileName = 'secrets.yml'
+
+    if not fileExists(secretsFileName):
+        print('No secrets file, using example data.')
+        return
+
+    global phoneNumbers
+    with open(secretsFileName,'r') as f:
+        secrets = yaml.safe_load(f)
+        phoneNumbers = secrets['phoneNumbers']
+    print(f"Loaded {len(phoneNumbers)} names/numbers")
+
+
+def getMissing(random_count):
     
     rand = []
     i = 0
@@ -25,7 +46,7 @@ def getMissing(random_count, missingNums = None):
 
     return rand
 
-def getPhoneNumber(rand):
+def getPhoneNumber(rand, number):
 
     phoneNumber = ''
     i = 0
@@ -40,44 +61,53 @@ def getPhoneNumber(rand):
     
     return phoneNumber
 
-def printLevelMessage(level):
+def printLevelMessage(level, name):
     msg = ''
     if level == 1:
         msg += f"We are missing {level} digits"
     else:
         msg += f'Now we are Missing {level} digits'
-    msg += ' of Mom\'s Phone Number.'
+    msg += f' of {name}\'s Phone Number.'
     print(msg)
 
-lose = False
-while difficulty < 10:
-    difficulty += 1
-    missingNums = getMissing(difficulty)
-    msg = ''
-    while len(missingNums) > 0 and lose == False:
-        clearScreen()
-        printLevelMessage(difficulty)
-        print(getPhoneNumber(missingNums))
-        if msg != '':
-            print(msg)
-        print('What is the FIRST missing Digit?')
-        while True:
-            key = getch.getch()
-            if int(key) == number[missingNums[0]]:
-                missingNums.pop(0)
-                break
-
-            else:
-                attempts -= 1
-                if attempts != 0:
-                    msg = f'Try again! {attempts} attempt(s) left.'
-                else:
-                    lose = True
-                break
+def game(name, number, attempts, difficulty):
     
-clearScreen()
-if lose:
-    msg = 'You Lose!'
-else:
-    msg = f'Correct {name} Phone Number is: {getPhoneNumber([-1])}'
-print(msg)
+    number = str(number)
+    number = number.replace("-","")
+    number = list(map(int,number))
+    lose = False
+    while difficulty < 10:
+        difficulty += 1
+        missingNums = getMissing(difficulty)
+        msg = ''
+        while len(missingNums) > 0 and lose == False:
+            clearScreen()
+            printLevelMessage(difficulty, name)
+            print(getPhoneNumber(missingNums, number))
+            if msg != '':
+                print(msg)
+            print('What is the FIRST missing Digit?')
+            while True:
+                key = getch.getch()
+                if int(key) == number[missingNums[0]]:
+                    missingNums.pop(0)
+                    break
+
+                else:
+                    attempts -= 1
+                    if attempts != 0:
+                        msg = f'Try again! {attempts} attempt(s) left.'
+                    else:
+                        lose = True
+                    break
+        
+    clearScreen()
+    if lose:
+        msg = 'You Lose!'
+    else:
+        msg = f'Correct {name}\'s Phone Number is: {getPhoneNumber([-1], number)}'
+    print(msg)
+
+loadSecretsFile()
+for each in phoneNumbers:
+    game(each['name'], each['number'], attempts, difficulty)
